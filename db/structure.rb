e434b847288e4a1c92a5a810312890a0edaff611
Oleg <oleg.saltykov@gmail.com>
2025-07-09 23:29:43 -0400

    deploy

--- db/structure.rb
+++ db/structure.rb
@@ -1,131 +1,4 @@
 STRUCTURE = {
-  users: {
-    id: {
-      generated: false,
-      allow_null: false,
-      default: nil,
-      db_type: "INTEGER",
-      primary_key: true,
-      auto_increment: true,
-      type: "integer",
-      ruby_default: nil
-    },
-    email: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    },
-    password: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    }
-  },
-  admin__user_admins: {
-    id: {
-      generated: false,
-      allow_null: false,
-      default: nil,
-      db_type: "INTEGER",
-      primary_key: true,
-      auto_increment: true,
-      type: "integer",
-      ruby_default: nil
-    },
-    email: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    },
-    password: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    }
-  },
-  admin_user_admins: {
-    id: {
-      generated: false,
-      allow_null: false,
-      default: nil,
-      db_type: "INTEGER",
-      primary_key: true,
-      auto_increment: true,
-      type: "integer",
-      ruby_default: nil
-    },
-    email: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    },
-    password: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    }
-  },
-  admin_sections: {
-    id: {
-      generated: false,
-      allow_null: false,
-      default: nil,
-      db_type: "INTEGER",
-      primary_key: true,
-      auto_increment: true,
-      type: "integer",
-      ruby_default: nil
-    },
-    title: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "varchar(255)",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil,
-      max_length: 255
-    },
-    description: {
-      generated: false,
-      allow_null: true,
-      default: nil,
-      db_type: "TEXT",
-      primary_key: false,
-      type: "string",
-      ruby_default: nil
-    }
-  },
   admin_documents: {
     id: {
       generated: false,
@@ -164,6 +37,69 @@ STRUCTURE = {
       primary_key: false,
       type: "integer",
       ruby_default: nil
+    }
+  },
+  admin_sections: {
+    id: {
+      generated: false,
+      allow_null: false,
+      default: nil,
+      db_type: "INTEGER",
+      primary_key: true,
+      auto_increment: true,
+      type: "integer",
+      ruby_default: nil
+    },
+    title: {
+      generated: false,
+      allow_null: true,
+      default: nil,
+      db_type: "varchar(255)",
+      primary_key: false,
+      type: "string",
+      ruby_default: nil,
+      max_length: 255
+    },
+    description: {
+      generated: false,
+      allow_null: true,
+      default: nil,
+      db_type: "TEXT",
+      primary_key: false,
+      type: "string",
+      ruby_default: nil
+    }
+  },
+  users: {
+    id: {
+      generated: false,
+      allow_null: false,
+      default: nil,
+      db_type: "INTEGER",
+      primary_key: true,
+      auto_increment: true,
+      type: "integer",
+      ruby_default: nil
+    },
+    email: {
+      generated: false,
+      allow_null: true,
+      default: nil,
+      db_type: "varchar(255)",
+      primary_key: false,
+      type: "string",
+      ruby_default: nil,
+      max_length: 255
+    },
+    password: {
+      generated: false,
+      allow_null: true,
+      default: nil,
+      db_type: "varchar(255)",
+      primary_key: false,
+      type: "string",
+      ruby_default: nil,
+      max_length: 255
     }
   }
 }
