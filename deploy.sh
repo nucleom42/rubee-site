547971f45e90f9cad9af2315c8e74ff3d2d84fa7
Oleg <oleg.saltykov@gmail.com>
2025-07-19 14:11:39 -0400

    deploy conditionally load rbenv

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-# Load rbenv if installed via shell config
 export HOME=/home/oleg
 export PATH="$HOME/.rbenv/bin:$PATH"
-eval "$(rbenv init - bash)"  # or "zsh" if using zsh
+
+# Load rbenv if it's installed
+if command -v rbenv >/dev/null 2>&1; then
+  eval "$(rbenv init - bash)"
+fi
 
 log() {
   echo "[$(date '+%H:%M:%S')] $*"
 }
-
-sudo chown -R oleg:oleg ~/.rbenv
 
 log "bundle install --redownload"
 bundle install --redownload
@@ -26,3 +27,4 @@ rubee stop || true
 RACK_ENV=production WEB_CONCURRENCY=3 rubee start > /dev/null 2>&1 &
 
 log "done"
+og "done"
