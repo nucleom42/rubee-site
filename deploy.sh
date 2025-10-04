06981d2273b467c9473c1b5608910d8cd6af9b64
Oleg <oleg.saltykov@gmail.com>
2025-10-04 16:46:30 -0400

    log

--- deploy.sh
+++ deploy.sh
@@ -47,6 +47,10 @@ RACK_ENV=production rubee db run:all
 # --- Restart Rubee server ---
 log "restarting rubee server"
 rubee stop || true
+
+rm -rf log/
+mkdir -p log
+touch log/production.log
 RACK_ENV=production WEB_CONCURRENCY=4 rubee start > log/production.log 2>&1 &
 
 log "done"
