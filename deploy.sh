ec1a87a5f94148c825b9d56a00a92151be4c97b5
Oleg <oleg.saltykov@gmail.com>
2026-03-28 21:48:20 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ set -e
 export HOME=/home/oleg
 export PATH="$HOME/.rbenv/bin:$PATH"
 
-# Load rbenv if it's installed
+# LLoad rbenv if it's installed
 if command -v rbenv >/dev/null 2>&1; then
   eval "$(rbenv init - bash)"
 fi
